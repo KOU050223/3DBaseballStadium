c9ba1604661f293a8c128e29875d01899cbcdc13
KOU050223 <kouhei20050223@icloud.com>
2025-08-26 02:16:54 +0900

    feat: FBXローダーフックを追加

---
+++ src/hooks/useFBXLoader.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useFBX } from '@react-three/drei';
+
+interface UseFBXLoaderProps {
+  modelPath: string;
+  scale?: [number, number, number];
+  position?: [number, number, number];
+  rotation?: [number, number, number];
+  onLoad?: () => void;
+  onError?: (error: Error) => void;
+}
+
+export const useFBXLoader = ({ 
+  modelPath, 
+  scale = [1, 1, 1],
+  position = [0, 0, 0],
+  rotation = [0, 0, 0],
+  onLoad, 
+  onError 
+}: UseFBXLoaderProps) => {
+  const fbx = useFBX(modelPath);
+  
+  useEffect(() => {
+    if (fbx) {
+      // 基本的なトランスフォーム設定
+      fbx.scale.set(scale[0], scale[1], scale[2]);
+      fbx.position.set(position[0], position[1], position[2]);
+      fbx.rotation.set(rotation[0], rotation[1], rotation[2]);
+      onLoad?.();
+    }
+  }, [fbx, scale, position, rotation, onLoad]);
+
+  return fbx;
+};
