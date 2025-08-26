8ee3917cbcbd23eb250e59425b72468c1f036ce2
KOU050223 <kouhei20050223@icloud.com>
2025-08-27 00:59:25 +0900

    modify: ページを分けた

---
+++ src/app/play/page.tsx
@@ -0,0 +1,10 @@
+// プレイ画面
+import { Scene } from '@/components/future/modelTest/Scene';
+
+export default function Play() {
+  return (
+    <div className="w-full h-[calc(100vh-80px)]">
+      <Scene debugMode={process.env.NODE_ENV === 'development'} />
+    </div>
+  )
+}
