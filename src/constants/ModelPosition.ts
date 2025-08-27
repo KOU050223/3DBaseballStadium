7e146ee162467dd32135f3ecb0e0304d6926e096
KOU050223 <kouhei20050223@icloud.com>
2025-08-27 23:35:05 +0900

    fix: サイズを定数ファイルとして分離

---
+++ src/constants/ModelPosition.ts
@@ -0,0 +1,46 @@
+// モデルの初期位置
+
+// 野球盤のモデル
+const StadiumModelPosition = {
+  x: 0,
+  y: 0,
+  z: 0
+};
+
+// モデルのスケール
+const StadiumModelScale = 1;
+
+// モデルの回転
+const StadiumModelRotation = {
+  x: 0,
+  y: 0,
+  z: 0
+};
+
+// バットのモデル
+const BatModelPosition = {
+  x: 0,
+  y: 0,
+  z: 0
+};
+
+const BatModelRotation = {
+  x: 0,
+  y: 0,
+  z: 0
+};
+
+const BatModelScale = 1;
+
+// ボールのモデル
+const BallModelScale = 1;
+
+export {
+  StadiumModelPosition,
+  StadiumModelScale,
+  StadiumModelRotation,
+  BatModelPosition,
+  BatModelRotation,
+  BatModelScale,
+  BallModelScale
+};
