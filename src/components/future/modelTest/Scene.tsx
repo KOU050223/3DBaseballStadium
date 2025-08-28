5729811e1e7dbc1349a2dc7eea9d97eb553c9d93
otonasi-muonn <nayudoutianzhong@gmail.com>
2025-08-28 11:38:00 +0900

    feat: カメラの向く先を設定、カメラの位置微調整

--- src/components/future/modelTest/Scene.tsx
+++ src/components/future/modelTest/Scene.tsx
@@ -29,9 +29,9 @@ export const Scene: React.FC<SceneProps> = ({ debugMode = false }) => {
   return (
     <div className="w-full h-full relative">
       <Canvas
-        camera={{ position: [0, 2, -4]}}
+        camera={{ position: [0, 1.5, -4]}}
       >
-        <OrbitControls />
+        <OrbitControls target={[0, 1.5, 0]} />
         <Environment preset="sunset" />
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} intensity={1} />
